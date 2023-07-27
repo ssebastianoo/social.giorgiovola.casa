2d82833ccc37893767fe4b882e64ca900fbab560
ssebastianoo <sebastianogirotto05@gmail.com>
2023-07-28 00:14:35 +0200

    make single post not clickable

--- src/lib/components/Posts.svelte
+++ src/lib/components/Posts.svelte
@@ -8,12 +8,26 @@
 
 <div class="posts">
 	{#each posts as post}
-		<Post {post} />
+		<a href={`/@${post.user.username}/${post.id}`} class="post-wrapper">
+			<Post {post} />
+		</a>
 	{/each}
 </div>
 
 <style lang="scss">
 	@import 'src/variables.scss';
+
+	.post-wrapper {
+		color: unset;
+		text-decoration: none;
+		border-radius: 5px;
+		padding: 10px;
+		display: block;
+
+		&:hover {
+			background-color: black;
+		}
+	}
 
 	.posts {
 		display: flex;
