ff5d3794dac7f24e51e285d449a96815632a8024
chickenmatty <44543605+chickenmatty@users.noreply.github.com>
2023-07-28 13:51:18 +0200

    add auth check in server hook

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { User } from "$lib/types";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+            user: User | null;
+        }
 		// interface PageData {}
 		// interface Platform {}
 	}
---
+++ src/hooks.server.ts
@@ -0,0 +1,35 @@
+import { sql } from '$lib/server/db';
+import type { Handle } from '@sveltejs/kit';
+
+export const handle = (async ({ event, resolve }) => {
+    const cookie = event.cookies.get('social.giorgiovola.casa-token');
+	if (!cookie) {
+		event.locals.user = null;
+        return await resolve(event);
+	}
+
+	const sessions = await sql`
+	SELECT user_id FROM sessions WHERE token = ${cookie}
+	`;
+
+	if (sessions.length === 0) {
+		event.locals.user = null;
+        return await resolve(event);
+	}
+
+	const res = await sql`
+	SELECT * FROM users WHERE id = ${sessions[0].user_id}
+	`;
+
+	const user = {
+		id: res[0].id,
+		email: res[0].email,
+		name: res[0].name,
+		username: res[0].username,
+		avatar: res[0].avatar,
+		created_at: res[0].created_at
+	};
+
+    event.locals.user = user;
+	return await resolve(event)
+}) satisfies Handle;
