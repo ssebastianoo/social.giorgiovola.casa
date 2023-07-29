6d1e15ee8c02ee12f03e3c61d8fab5cc406fea81
ssebastianoo <sebastianogirotto05@gmail.com>
2023-07-29 13:48:03 +0200

    show top links

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -87,7 +87,7 @@
 						<path
 							d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z"
 							fill={post.liked ? '#bb5353' : 'transparent'}
-							stroke={post.liked ? '#bb5353' : 'white'}
+							stroke={post.liked ? '#bb5353' : '#d9d9d9'}
 						/>
 					</g></svg
 				>
@@ -119,7 +119,7 @@
 
 		.like:not([data-liked='true']):not([data-logged='false']):hover {
 			svg path {
-				fill: white;
+				fill: $color3;
 			}
 		}
 
@@ -157,7 +157,7 @@
 			flex-direction: column;
 			width: 100%;
 			gap: 3px;
-			
+
 			.actions {
 				margin-top: 10px;
 				display: flex;
