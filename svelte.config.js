6d4fd224c3e099eb1043d97c7acd40575a031c0b
chickenmatty <44543605+chickenmatty@users.noreply.github.com>
2023-07-30 12:51:44 +0200

    content security policy

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,30 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter(),
+        csp: {
+            mode: 'auto',
+            directives: {
+                'default-src': ['none'],
+                'script-src': ['self'],
+                'style-src': ['self'],
+                'connect-src': ['self'],
+                'img-src': ['self', 'source.boringavatars.com'],
+                'base-uri': ['self'],
+                'form-action': ['self'],
+                'manifest-src': ['self'],
+                'font-src': ['self'],
+            }
+        }
+    }
 };
 
 export default config;
