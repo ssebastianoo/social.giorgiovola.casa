81a3b6fd5a0c6ad38080a3ba5569637b3989de1a
chickenmatty <44543605+chickenmatty@users.noreply.github.com>
2023-08-18 13:54:58 +0200

    add notifications triggers

--- src/schema.sql
+++ src/schema.sql
@@ -29,3 +29,66 @@ CREATE TABLE IF NOT EXISTS likes (
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     PRIMARY KEY (user_id, post_id)
 );
+
+CREATE TABLE IF NOT EXISTS inbox(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    sender_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+    type text NOT NULL CHECK (type IN ('mention', 'reply', 'like')),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    read_at TIMESTAMPTZ,
+    UNIQUE (user_id, sender_id, post_id, type)
+);
+
+CREATE OR REPLACE FUNCTION notify_like() RETURNS TRIGGER AS $notify_like$
+    BEGIN
+        INSERT INTO inbox(user_id, sender_id, post_id, type)
+        VALUES (
+            (SELECT posts.user_id FROM posts WHERE id = NEW.post_id),
+            NEW.user_id,
+            NEW.post_id,
+            'like'
+        ) ON CONFLICT DO NOTHING;
+    RETURN NULL;
+    END;
+$notify_like$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS likes_notification ON likes;
+CREATE TRIGGER likes_notification
+    AFTER INSERT ON likes
+    FOR EACH ROW EXECUTE function notify_like();
+
+CREATE OR REPLACE FUNCTION notify_posts() RETURNS TRIGGER AS $notify_posts$
+    DECLARE
+        matches text[];
+        match text;
+    BEGIN
+        matches = (select regexp_matches(NEW.content, '@([a-zA-Z0-9_]+)(?:\s|$)', 'g'));
+        IF NEW.reply_to IS NOT NULL then
+            INSERT INTO inbox(user_id, sender_id, post_id, type)
+            VALUES (
+                    (SELECT posts.user_id FROM posts WHERE id=NEW.reply_to),
+                    NEW.user_id,
+                    NEW.id,
+                    'reply'
+                   ) ON CONFLICT DO NOTHING;
+        FOREACH match in ARRAY matches
+            LOOP
+                INSERT INTO inbox(user_id, sender_id, post_id, type)
+                VALUES (
+                (SELECT users.id FROM users where username = match),
+                NEW.user_id,
+                NEW.id,
+                'mention'
+                ) ON CONFLICT DO NOTHING ;
+            end loop;
+        end if;
+    RETURN NULL;
+    END;
+$notify_posts$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS posts_notification ON posts;
+CREATE TRIGGER posts_notification
+    AFTER INSERT ON posts
+    FOR EACH ROW EXECUTE function notify_posts();
