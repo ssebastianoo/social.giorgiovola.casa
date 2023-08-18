371d044bb50be143d73ba0996ef096f610c40c0a
chickenmatty <44543605+chickenmatty@users.noreply.github.com>
2023-08-18 22:58:25 +0200

    css, read noti on click

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,4 +2,5 @@ import { writable } from 'svelte/store';
 import type { User } from './types';
 
 export const user = writable<User | null>(null);
+export const notisCount = writable<number>(0);
 export const isMobile = writable(false);
