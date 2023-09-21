f1ef145bf741f980d36a5dcc1500ee876a2b3b21
chickenmatty <44543605+chickenmatty@users.noreply.github.com>
2023-09-22 00:03:36 +0200

    post new posts to discord

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,5 +1,7 @@
 import type { RequestHandler } from './$types';
 import { sql } from '$lib/server/db';
+import { DISCORD_WEBHOOK_URL } from '$env/static/private';
+import { PUBLIC_URL } from '$env/static/public';
 
 export const POST: RequestHandler = async ({ locals, request }) => {
 	if (!locals.user) {
@@ -26,14 +28,25 @@ export const POST: RequestHandler = async ({ locals, request }) => {
 		RETURNING content, created_at, edited_at, id, (SELECT jsonb_build_object(
             'id', p.id,
             'username', u.username
-        ) FROM posts p INNER JOIN users u ON p.user_id = u.id WHERE p.id = posts.reply_to) AS reply_to
+        ) FROM posts p INNER JOIN users u ON p.user_id = u.id WHERE p.id = posts.reply_to) AS reply_to, (SELECT jsonb_build_object(
+            'id', u.id,
+            'username', u.username,
+            'name', u.name,
+            'avatar', u.avatar
+        ) FROM users u WHERE u.id = user_id) AS user
             
 	`;
-	const users = await sql`
-		SELECT id, name, username, email, avatar FROM users WHERE id = ${locals.user.id}
-	`;
-	posts[0].user = users[0];
-
+	if (!reply_to) {
+		await fetch(DISCORD_WEBHOOK_URL, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				content: `${PUBLIC_URL}/@${posts[0].user.username}/${posts[0].id}`
+			})
+		});
+	}
 	return new Response(JSON.stringify(posts[0]));
 };
 
